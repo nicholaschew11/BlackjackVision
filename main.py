f6eefb3989ae9e03598a041b3fb3061c8f80b7c9
n <chew.nicky@gmail.com>
2022-11-17 16:33:37 -0500

    .

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import os
+import time
+import cv2
+
 from src.CardDetection import CardDetection
 
 if __name__ == "__main__":
     cardDetection = CardDetection()
     cardDetection.run()
+
+    #Might need to put code below in a separate thread
+
+    # loop = 0
+    # while (loop == 0):
+    #     for i, card in enumerate():
+    #         print(card)
+
+    #     time.sleep(1)
+
+    #     key = cv2.waitKey(1) & 0xFF
+    #     if key == ord("q"):
+    #         cam_quit = 1
