90eed66f29b08e6529185b518841eb75dc883bb9
Daniel <yedaniel99@gmail.com>
2022-11-26 22:12:02 -0500

    the class used to retrieve the optimal moves

---
+++ src/test.py
@@ -0,0 +1,18 @@
+from blackjack import blackjack
+
+class Test:
+    def __init__(self, strategy_name, cards):
+        self.strategy_name = strategy_name
+
+        print("Player strategy:", self.strategy_name)
+
+        this_table = blackjack.Table(4, 0.75)
+        this_table.shoe.cards=[("2d",[2],"2")]
+        for card in cards:
+            this_table.shoe.cards.append(card)
+
+        new_deck = blackjack.Deck()
+
+        this_table.shoe.cards = this_table.shoe.cards + new_deck.cards
+
+        return this_table.play_one_round(self.strategy_name)    # Play a game.
