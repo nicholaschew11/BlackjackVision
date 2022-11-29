b3b2f598d0738514b605a79f6de966ea57f9d635
Mihir Kachroo <mkachroo29@gmail.com>
2022-11-29 18:01:05 -0500

    Added text display functionality

--- src/OLED/oled.py
+++ src/OLED/oled.py
@@ -25,6 +25,28 @@ try:
     logging.info("clear display")
     disp.clear()
 
+    # Create blank image for drawing.
+    image1 = Image.new('RGB', (disp.width, disp.height), 0)
+    draw = ImageDraw.Draw(image1)
+    font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 12)
+    font1 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+    font2 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    logging.info ("***draw line")
+    draw.line([(0,0),(127,0)], fill = "RED")
+    draw.line([(0,0),(0,127)], fill = "RED")
+    draw.line([(0,127),(127,127)], fill = "RED")
+    draw.line([(127,0),(127,127)], fill = "RED")
+    logging.info ("***draw text")
+    draw.text((20,0), 'This ', font = font1, fill = "BLUE")
+    draw.text((20,24), u'Is ', font = font2, fill = "MAGENTA")
+    draw.text((20,64), 'Pretty ', font = font1, fill = "CYAN")
+    draw.text((20,92), u'Cool ', font = font2, fill = "GREEN")
+    image1 = image1.rotate(0)
+    disp.ShowImage(disp.getbuffer(image1))
+    time.sleep(3)
+
+    disp.clear()
+
 except IOError as e:
     logging.info(e)
     
