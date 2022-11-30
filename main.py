ad819e671dfa9b606dc24a8daebf02f41bdb531d
Mihir Kachroo <mkachroo29@gmail.com>
2022-11-29 20:17:25 -0500

    Added oled into main

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 import time
 import cv2
+from file import function
+import sys
+
+sys.path.insert(1, '/OLED')
 
 from src.CardDetection import CardDetection
 
+from oled import displayMessage
+
+
 if __name__ == "__main__":
+    displayMessage()
     cardDetection = CardDetection()
     cardDetection.run()
